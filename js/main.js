ce0e0b950e7cd8c79f0c9918203334796db24ee4
Shamadruu <jacksonayres11@gmail.com>
2020-07-26 17:45:38 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1383,7 +1383,7 @@ City.prototype.update = function(){
 	if (this.disease > 100) {
 		this.disease = 100;
 	}
-	this.population = Math.round((this.population - (this.disease * this.infra) - (10 * this.crime * this.infra) + 25)*(1 + Math.log(this.age/15)));
+	this.population = Math.round((this.population - (this.disease * this.infra * 10) - (10 * this.crime * this.infra) - 25)*(1 + Math.log(this.age/15)));
 	if (this.population < 0) {
 		this.population = 0;
 	}
