d1223eddf229d12aaa456c06d7316c8c497c548f
Shamadruu <jacksonayres11@gmail.com>
2020-07-27 13:49:28 -0600

    Create projects.js

---
+++ js/projects.js
@@ -0,0 +1,212 @@
+//Read-only project data
+	var  projects = {
+		advancedCityPlanning : {
+			built: false,
+			cost: {
+				uranium: 10000,
+				aluminum: 40000,
+				steel: 20000,
+				munitions: 20000,
+				food: 2500000
+			}
+		},
+		armsStockpile : {
+			built: false, 
+			cost: {
+				aluminum: 125,
+				steel: 125,
+				money: 4000000
+			}
+		},
+		bauxiteworks: {
+			built: false, 
+			cost: {
+				steel: 750,
+				gasoline: 1500,
+				money: 5000000
+			}
+		},
+		civilEngineering: {
+			built: false, 
+			cost: {
+				oil: 1000,
+				iron: 1000,
+				bauxite: 1000,
+				money: 3000000
+			}
+		},
+		cityPlanning : {
+			built: false,
+			cost: {
+				coal: 10000,
+				oil: 10000,
+				aluminum: 20000,
+				munitions: 10000,
+				gasoline: 10000,
+				food: 1000000
+			}
+		},
+		cia: {
+			built: false, 
+			cost: {
+				steel: 500,
+				gasoline: 500,
+				money: 5000000
+			}
+		},
+		greenTechnologies : {
+			built: false,
+			cost: {
+				iron: 10000,
+				steel: 10000,
+				aluminum: 10000,
+				food: 250000,
+				money: 100000000
+			}
+		},
+		gasolineReserve: {
+			built: false, 
+			cost: {
+				aluminum: 125,
+				steel: 125,
+				money: 4000000
+			}
+		},
+		tradeCenter: {
+			built: false, 
+			cost: {
+				aluminum: 2500,
+				steel: 2500,
+				gasoline: 5000,
+				money: 4500000
+			}
+		},
+		ironDome: {
+			built: false,
+			cost: {
+				aluminum: 500,
+				steel: 1250,
+				gasoline: 500,
+				money: 6000000
+			}
+		},
+		ironworks: {
+			built: false, 
+			cost: {
+				aluminum: 750,
+				steel: 1500,
+				money: 5000000
+			}
+		},
+		irrigation: {
+			built: false, 
+			cost: {
+				steel: 500,
+				aluminum: 500,
+				money: 3000000
+			}
+		},
+		launchPad: {
+			built: false, 
+			cost: {
+				steel: 1000,
+				gasoline: 350,
+				money: 8000000
+			}
+		},
+		moonLanding : {
+			built: false,
+			cost: {
+				oil: 5000,
+				munitions: 5000,
+				gasoline: 5000,
+				steel: 5000,
+				aluminum: 5000,
+				uranium: 10000,
+				money: 5000000
+			}
+		},
+		nuclearResearch : {
+			built: false,
+			cost: {
+				steel: 1000,
+				gasoline: 7500,
+				money: 50000000
+			}
+		},
+		pirateEconomy: {
+			built: false,
+			cost: {
+				aluminum: 10000,
+				munitions: 10000,
+				gasoline: 10000,
+				steel: 10000,
+				money: 25000000
+			}
+		},
+		propaganda: {
+			built: false, 
+			cost: {
+				aluminum: 1500,
+				money: 15000000
+			}
+		},
+		recycling: {
+			built: false,
+			cost: {
+				food: 100000,
+				money: 1000000
+			}
+		},
+		spaceProgram : {
+			built: false,
+			cost: {
+				uranium: 20000,
+				oil: 20000,
+				iron: 10000,
+				gasoline: 5000,
+				steel: 1000,
+				aluminum: 1000,
+				money: 40000000
+			}
+		},
+		spySattelite : {
+			built: false,
+			cost: {
+				oil: 10000,
+				iron: 10000,
+				lead: 10000,
+				bauxite: 10000,
+				uranium: 10000,
+				money: 20000000,
+			}
+		},
+		teleSattelite: {
+			built: false,
+			cost: {
+				uranium: 10000,
+				iron: 10000,
+				oil: 10000,
+				aluminum: 10000,
+				money: 300000000
+			}
+		},
+		uraniumEnrichment: {
+			built: false, 
+			cost: {
+				aluminum: 1000,
+				gasoline: 1000,
+				uranium: 500,
+				money: 21000000
+			}
+		},
+		vitalDefense: {
+			built: false, 
+			cost: {
+				steel: 3000,
+				gasoline: 5000,
+				aluminum: 3000,
+				money: 40000000
+			}
+		}
+	};
